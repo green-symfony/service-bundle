0bfb5d4e65af366e146c883f47f699b7e0605f26
Grin <son5-29@mail.ru>
2023-12-02 17:49:53 +1200

    add(abstract_patternable)

--- src/Service/ArrayService.php
+++ src/Service/ArrayService.php
@@ -7,6 +7,50 @@ class ArrayService
 	public function __construct() {}
 	
     //###> API ###
+	
+	/*###> FILTERING FOR REALIZATION OF \GS\Command\Trait\PatternAbleInstance ### */
+	/* FILTERING FOR REALIZATION OF \GS\Command\Trait\PatternAbleInstance
+		
+		-> AbstractPatternAbleCommandUseOneThreeReplacementPartsOfRegexTrait
+	
+		Gets the first result.
+
+        If pass all the possible keys into $primaryKeysValues
+			you'll get [] guaranteed!
+
+        forFilter: false (for constructing aims)
+            returns pattern diff array when there were interesections
+            RETURNS [] WHEN THERE IS NO DIFFERENCE BETWEEN PATTERN DATA AND PASSED ONES
+            RETURNS null when wasn't intersection passed $primaryKeysValues with passed $patterns
+            Если $pattern имеет какие-то различия от $primaryKeysValues возвращает не пустой массив
+		forFilter: true (for filtering aims)
+            returns [] when pattern was matched
+            returns null when pattern was NOT matched
+    */
+    public function getParsedOneThreeReplacementPartsRegex(
+        array $primaryKeysValues,
+        array $patterns,
+        bool $forFilter = false,
+    ): ?array {
+        foreach ($patterns as $pattern) {
+            $diff = \array_diff_assoc($pattern, $primaryKeysValues);
+            //###> Goal forFilter: return [] if it's possible (there is no difference with $pattern)
+            if ($forFilter) {
+                if (empty($diff)) {
+                    return [];
+                }
+                continue;
+            }
+            // Are there any coincidences?
+            $result = \array_intersect_assoc($primaryKeysValues, $pattern);
+            //###> Return the difference related $pattern
+            if (!empty($result)) {
+                return $diff;
+            }
+        }
+        return null;
+    }
+	/*###< FILTERING FOR REALIZATION OF \GS\Command\Trait\PatternAbleInstance ### */
 
     public static function getKeyValueString(
         array $keyValue,
